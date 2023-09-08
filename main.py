c82d6f284fb4131b5b50b200ba241253d0654f90
Guy-DeBush <derek_fon_ferrum@mail.ru>
2023-09-08 21:32:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,16 @@ bot = telebot.TeleBot('6523416756:AAHOOrzXq35PnIX07vv0j3Shj1PZMnliOp8')
 def start(message):
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Начинай,Данечка")
+    btn1 = types.KeyboardButton("Кнопочка для игры")
     markup.add(btn1)
     bot.send_message(message.from_user.id, "Привет! Я заебался,но вроде сделал!", reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
-    if message.text == 'Начинай,Данечка':
+    if message.text == 'Кнопочка для игры':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание кнопочки
-        btn1 = types.KeyboardButton('Игра')
-        markup.add(btn1)
-        bot.send_message(message.from_user.id, 'Бот загадал число от 1 до 10', reply_markup=markup) #ответ бота
+        bot.send_message(message.from_user.id, 'Бот загадал число от 1 до 10, угадывай!', reply_markup=markup) #ответ бота
         Game_answer = message.text
         Bot_number = random.randint(1,10)
         if Game_answer == Bot_number:

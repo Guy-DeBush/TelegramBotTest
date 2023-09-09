0ed30549bba6994175e6c60f1185d412623fd1b9
Guy-DeBush <derek_fon_ferrum@mail.ru>
2023-09-09 23:37:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot import types
 import random
 
 
@@ -8,14 +7,18 @@ bot = telebot.TeleBot('6523416756:AAHOOrzXq35PnIX07vv0j3Shj1PZMnliOp8')
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    bot.send_message(message.chat.id, "Привет! Я заебался,но вроде сделал! Кидай число от 1 до 100" )
 
-    bot.send_message(message.from_user.id, "Привет! Я заебался,но вроде сделал! Кидай число от 1 до 100" )
-    User_answer = message.text
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    User_answer = message.text.lower()
+    chat_id = message.chat.id
     Bot_number = random.randint(1, 10)
+    bot.send_message(message.chat.id, "Привет! Я заебался,но вроде сделал! Кидай число от 1 до 100")
     if User_answer == Bot_number:
-        bot.send_message(message.from_user.id, "Угадал!")
+        bot.send_message(chat_id, "Угадал!")
     else:
-        bot.send_message(message.from_user.id, "ЛОХ! ПОПУСК! НЕ УГАДАЛ!")
+        bot.send_message(chat_id, "ЛОХ! ПОПУСК! НЕ УГАДАЛ!")
 
 
 bot.polling(none_stop=True, interval=0) #Можно написать и инфинити поллинг, но бля так саснее

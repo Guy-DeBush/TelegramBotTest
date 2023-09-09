b8eb163e858ef6308e79a6cfb5c3b5ddb7c325de
Guy-DeBush <derek_fon_ferrum@mail.ru>
2023-09-09 03:17:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,17 +18,13 @@ def start(message):
 def get_text_messages(message):
     if message.text == 'Кнопочка для игры':
         bot.send_message(message.from_user.id, 'Бот загадал число от 1 до 10, угадывай!') #ответ бота
-        User_answer = message.chat.id
+        User_answer = message.text
         YourNum = bot.send_message(User_answer, 'Твоё число:')
-        bot.register_next_step_handler(YourNum, step_Set_Price)
         Bot_number = random.randint(1,10)
         if YourNum == Bot_number:
             bot.send_message(message.from_user.id,"Угадал!")
         else:
             bot.send_message(message.from_user.id,"ЛОХ! ПОПУСК! НЕ УГАДАЛ!")
 
-def step_Set_Price(message):
-    User_answer = message.chat.id
-    userPrice= message.text
 
 bot.polling(none_stop=True, interval=0)

a4f732442172bbe8acf443f748c268bdc4677a21
Guy-DeBush <derek_fon_ferrum@mail.ru>
2023-09-09 23:48:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ bot = telebot.TeleBot('6523416756:AAHOOrzXq35PnIX07vv0j3Shj1PZMnliOp8')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Я заебался,но вроде сделал! Кидай число от 1 до 100" )
+    bot.send_message(message.chat.id, "Привет! Я заебался,но вроде сделал! Кидай число от 1 до 10" )
 
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
     User_answer = message.text.lower()
     chat_id = message.chat.id
     Bot_number = random.randint(1, 10)
-    bot.send_message(message.chat.id, "Привет! Я заебался,но вроде сделал! Кидай число от 1 до 100")
+    bot.send_message(chat_id, f'Закаданное число: {Bot_number}')
     if User_answer == Bot_number:
         bot.send_message(chat_id, "Угадал!")
     else:

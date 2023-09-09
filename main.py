0b70e3c3d5fab379132fd55e0b862dafdb9a3be5
Guy-DeBush <derek_fon_ferrum@mail.ru>
2023-09-10 00:05:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def text_handler(message):
     User_answer = message.text.lower()
     chat_id = message.chat.id
     Bot_number = random.randint(1, 10)
-    bot.send_message(chat_id, f'Закаданное число: {Bot_number}')
+    bot.send_message(chat_id, f'Загаданное число: {Bot_number}')
+    bot.send_message(chat_id, f'Число игрока: {User_answer}')
     if User_answer == Bot_number:
         bot.send_message(chat_id, "Угадал!")
     else:
         bot.send_message(chat_id, "ЛОХ! ПОПУСК! НЕ УГАДАЛ!")
 
 
-bot.polling(none_stop=True, interval=0) #Можно написать и инфинити поллинг, но бля так саснее
+bot.infinity_polling()
 

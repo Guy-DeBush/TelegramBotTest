5b133b5e85388420b230a8bfdff5e08541de0bdd
Guy-DeBush <derek_fon_ferrum@mail.ru>
2023-09-09 22:47:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,24 +9,13 @@ bot = telebot.TeleBot('6523416756:AAHOOrzXq35PnIX07vv0j3Shj1PZMnliOp8')
 @bot.message_handler(commands=['start'])
 def start(message):
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Кнопочка для игры")
-    markup.add(btn1)
-    bot.send_message(message.from_user.id, "Привет! Я заебался,но вроде сделал!", reply_markup=markup)
-
-@bot.message_handler(commands=['game'])
-def get_text_messages(message):
-    if message.text == 'Кнопочка для игры':
-        bot.send_message(message.from_user.id, 'Бот загадал число от 1 до 10, угадывай!') #ответ бота
-        User_answer = message.text
-        bot.register_next_step_handler(User_answer, test)
-
-def test(message):
-    Bot_number = random.randint(1,10)
-    if message.text == Bot_number:
-        bot.send_message(message.from_user.id,"Угадал!")
+    bot.send_message(message.from_user.id, "Привет! Я заебался,но вроде сделал! Кидай число от 1 до 100" )
+    User_answer = message.text
+    Bot_number = random.randint(1, 10)
+    if User_answer == Bot_number:
+        bot.send_message(message.from_user.id, "Угадал!")
     else:
-        bot.send_message(message.from_user.id,"ЛОХ! ПОПУСК! НЕ УГАДАЛ!")
+        bot.send_message(message.from_user.id, "ЛОХ! ПОПУСК! НЕ УГАДАЛ!")
 
 
 bot.polling(none_stop=True, interval=0) #Можно написать и инфинити поллинг, но бля так саснее
